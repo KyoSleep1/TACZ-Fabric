ae1099e164716bae2313653f5caea2650d1d3f0e
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-04-25 12:19:31 -0700

    refactor: removed useless compat classes

--- src/main/java/com/tacz/guns/init/CompatRegistry.java
+++ src/main/java/com/tacz/guns/init/CompatRegistry.java
@@ -1,18 +1,14 @@
 package com.tacz.guns.init;
 
-import com.tacz.guns.compat.carryon.BlackList;
 import com.tacz.guns.compat.iris.IrisCompat;
-import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class CompatRegistry {
     public static final String CLOTH_CONFIG = "cloth-config";
     public static final String IRIS = "iris";
-    public static final String CARRY_ON_ID = "carryon";
 
     public static void init() {
         checkModLoad(IRIS, IrisCompat::initCompat);
-        checkModLoad(CARRY_ON_ID, BlackList::addBlackList);
     }
 
     public static void checkModLoad(String modId, Runnable runnable) {
