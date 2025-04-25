de86e27161cf3db367000fb41c876b16a5a985bd
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-04-25 13:38:02 -0700

    feat: added BaseEvent from PortLib

--- src/main/java/com/tacz/guns/api/event/GunBaseEvent.java
+++ src/main/java/com/tacz/guns/api/event/GunBaseEvent.java
@@ -1,7 +1,5 @@
 package com.tacz.guns.api.event;
 
-import io.github.fabricators_of_create.porting_lib.core.event.BaseEvent;
-
 public abstract class GunBaseEvent extends BaseEvent {
     public boolean post() {
         sendEvent();
