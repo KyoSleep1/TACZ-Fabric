0d7d5a118e90b0e35722d38f4353f12ad121f556
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-05-01 15:02:07 -0700

    Update NetworkClientHandler.java

--- src/main/java/com/tacz/guns/network/NetworkClientHandler.java
+++ src/main/java/com/tacz/guns/network/NetworkClientHandler.java
@@ -9,7 +9,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.util.Identifier;
 
-@Environment(EnvType.CLIENT)
 @SuppressWarnings("deprecation")
 public class NetworkClientHandler {
 
